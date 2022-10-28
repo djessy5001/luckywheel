1eb1691c91ded4f00a7320e9894847867ad9813c
djessyczaplicki <djessyczaplicki@gmail.com>
2022-10-28 21:48:46 +0200

    Initial commit

---
+++ src/main/java/com/servegame/yeyyyyyy/luckywheel/LuckyWheel.kt
@@ -0,0 +1,13 @@
+package com.servegame.yeyyyyyy.luckywheel
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LuckyWheel : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}

9e0532eba3ef76d686d733efd181721293f86ea7
djessyczaplicki <djessyczaplicki@gmail.com>
2023-01-10 20:49:46 +0100

    #8 Fixed can't add more than one enchanted book to the same LootTable

--- src/main/java/com/servegame/yeyyyyyy/luckywheel/extensions/Extensions.kt
+++ src/main/java/com/servegame/yeyyyyyy/luckywheel/extensions/Extensions.kt
@@ -6,6 +6,8 @@ import org.bukkit.ChatColor
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.EnchantmentStorageMeta
+import org.bukkit.inventory.meta.ItemMeta
 
 
 fun FileConfiguration.getColoredString(string: String): String {
@@ -38,7 +40,33 @@ fun ItemStack.toText(): String {
  * Returns true if the given item matches this instance's [ItemStack.type], [ItemStack.amount] and [ItemStack.getEnchantments]
  */
 fun ItemStack.matches(item: ItemStack): Boolean {
-    return this.type == item.type && this.amount == item.amount && this.enchantments == item.enchantments
+    if (item.itemMeta?.isEnchantedBook() == true && this.itemMeta?.isEnchantedBook() == true) {
+        val thisEnchantments = (this.itemMeta as EnchantmentStorageMeta)
+        val itemEnchantments = (item.itemMeta as EnchantmentStorageMeta)
+        if (thisEnchantments.hasStoredEnchants() && itemEnchantments.hasStoredEnchants()) {
+            val thisEnchantsMap = thisEnchantments.mapEnchantments()
+            val itemEnchantsMap = itemEnchantments.mapEnchantments()
+            return this.type == item.type && this.amount == item.amount && thisEnchantsMap == itemEnchantsMap
+        }
+    }
+    return this.type == item.type && this.amount == item.amount
+}
+
+fun ItemMeta.isEnchantedBook(): Boolean {
+    return this is EnchantmentStorageMeta
+}
+
+/**
+ * Goes through all the enchantments of an item and returns a map
+ * @return a map containing the enchantments names and levels like so:
+ * {
+ *  "minecraft:looting": 3,
+ *  "minecraft:sharpness": 5,
+ *  "minecraft:unbreaking": 1
+ * }
+ */
+fun EnchantmentStorageMeta.mapEnchantments(): Map<String, Int> {
+    return this.storedEnchants.mapKeys { bookEnchantment -> bookEnchantment.key.key.toString() }
 }
 
 fun Player.currentLootTable(): String = this.getMetadata("luckywheel_loot_table")[0].value().toString()

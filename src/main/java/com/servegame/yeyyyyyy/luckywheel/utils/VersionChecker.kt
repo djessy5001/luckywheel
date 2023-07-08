06b4c8816db1590ac5c778452efd92f4b3ae85b4
Djessy Czaplicki <djessyczaplicki@gmail.com>
2023-07-08 23:23:56 +0200

    Added compatibility for 1.20

--- src/main/java/com/servegame/yeyyyyyy/luckywheel/utils/VersionChecker.kt
+++ src/main/java/com/servegame/yeyyyyyy/luckywheel/utils/VersionChecker.kt
@@ -1,15 +1,12 @@
 package com.servegame.yeyyyyyy.luckywheel.utils
 
-import com.servegame.yeyyyyyy.luckywheel.utils.versions.V1_16
-import com.servegame.yeyyyyyy.luckywheel.utils.versions.V1_17
-import com.servegame.yeyyyyyy.luckywheel.utils.versions.V1_18
-import com.servegame.yeyyyyyy.luckywheel.utils.versions.V1_19
+import com.servegame.yeyyyyyy.luckywheel.utils.versions.*
 import org.bukkit.Bukkit
 
 class VersionChecker {
     companion object {
         val serverVersion = Bukkit.getServer().version
-        val VERSIONS = listOf(V1_16.VERSION, V1_17.VERSION, V1_18.VERSION, V1_19.VERSION)
+        val VERSIONS = listOf(V1_16.VERSION, V1_17.VERSION, V1_18.VERSION, V1_19.VERSION, V1_20.VERSION)
 
         fun checkVersion(version: String): Boolean {
             return Bukkit.getServer().version.contains(version)
